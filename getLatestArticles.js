3a431c51b81ac8da2ec8b1d48563ff6879136932
Juliano Rafael <julianorsfarias@msn.com>
2019-03-12 11:37:31 -0300

    :bug: Strip out tags on title

--- getLatestArticles.js
+++ getLatestArticles.js
@@ -8,7 +8,7 @@ const X = Xray({
   }
 })
 
-module.exports = async tag => {
+const getLatestArticles = async tag => {
   const articles = await X(
     `https://dev.to/t/${tag}/latest`,
     "#substories .single-article",
@@ -25,6 +25,15 @@ module.exports = async tag => {
     ]
   ).then(articles => articles.filter(article => article.title))
 
+  // clean tags out of title
+  for (article of articles) {
+    article.tags.forEach(tag => {
+      if (article.title.indexOf(tag) > -1) {
+        article.title = article.title.split(tag)[1].trim()
+      }
+    })
+  }
+
   // get twitter handle
   for (article of articles) {
     const socialLinks = await X(article.author.link, [
@@ -39,3 +48,5 @@ module.exports = async tag => {
 
   return articles
 }
+
+module.exports = getLatestArticles
